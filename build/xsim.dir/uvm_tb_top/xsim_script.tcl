1b530bf6fe2ec3e0fec57c50d1cf0435b14d8f69
Tahmeed Reza <tahmeedreza@gmail.com>
2025-11-17 13:13:22 +0000

    clean up

--- build/xsim.dir/uvm_tb_top/xsim_script.tcl
+++
@@ -1,4 +0,0 @@
-set_param project.enableReportConfiguration 0
-load_feature core
-current_fileset
-xsim {uvm_tb_top} -testplusarg UVM_VERBOSITY=UVM_LOW -testplusarg TESTNAME=base_test -testplusarg CLKFREQMHZ= -autoloadwcfg
